fd243d9d4d7abcff51c8a79bbe2116f107c93c12
Owen Wurst <113931506+OWurst@users.noreply.github.com>
2023-01-04 18:07:53 -0500

    Update Vehicle.java

--- Vehicle.java
+++ Vehicle.java
@@ -1,25 +1,19 @@
 public class Vehicle {
-	//these variables are part of the public class itself but are PRIVATE meaning they cant be directly seen or accessed by a program making a Vehicle object, they are using "data hiding"
-	// to access or set these we will need to make an INTERFACE to the class, ie public methods to get and set them - java calls this practice "getters and setters"
-  public String vclass = "";
-  public String make = "";
+	public String vclass = "";
+  	public String make = "";
 	public String model = "";
 	public int weight = 0;
 	public String color = "";
 	public double mpg = 0.0;
 	public int year = 0000;
 	public String notes = "System notes: \n";
-	//Constructor allow us to create an OBJECT instance of our class in various ways
-
-	//This is our base constructor, it will just allow an object to be created with a 'new Vehicle()' command. To set the datafields we will need to call the methods ourself.
+	
 	public Vehicle(){
 	}
-	// This is also a constructor of the same name but declared differently with an ability to take in parameters. This is 'Constructor Overloading'
-	public Vehicle(String vclass, String inmake, String inmodel,int inweight,String incolor,double inmpg,int inyear){
-		//This constructor can take in all the needed info for a car to be defined in the line declaring the object. It will call the setters.
-		// we could access the data fields above directly 'mpg = inmpg;' for example, but we would like to still apply our datahiding rules
-    setVClass(vclass);
-    setMake(inmake);
+		public Vehicle(String vclass, String inmake, String inmodel,int inweight,String incolor,double inmpg,int inyear){
+		
+		setVClass(vclass);
+    		setMake(inmake);
 		setModel(inmodel);
 		setWeight(inweight);
 		setColor(incolor);
@@ -29,24 +23,19 @@ public class Vehicle {
 
 
 
-  public void setVClass(String in){
-    vclass = in.toUpperCase();
-  }
-  public String getVClass(){
-    return vclass;
-  }
+  	public void setVClass(String in){
+    		vclass = in.toUpperCase();
+  	}
+  	public String getVClass(){
+    		return vclass;
+  	}
 
 	public  void setModel(String in){
-		//setter methods usually are VOID datatype as they only act to take in a value without giving anything back
-		// what if I need to make sure model names are stored as uppercase only? If I make the data fields public I cant enforce a rule like that. By hiding the data and using an interface i can
 		model = in.toUpperCase();
 	}
 
 	public  String getModel(){
-		//getters datatype for the method needs to match the datatype of the field it will return
-		//notice it isnt taking IN any parameters? It doesnt need to for this case
 		return model;
-
 	}
 
 	public void setMake(String in){
@@ -64,8 +53,6 @@ public class Vehicle {
 			notes += "The user entered an invalid weight of " + in + " as a weight \n";
 		}
 		weight = in;
-
-
 	}
 	public  int getWeight(){
 		return weight;
@@ -98,7 +85,7 @@ public class Vehicle {
 
 
 	public void stats(){
-    System.out.println("Vehicle class is " + vclass);
+    		System.out.println("Vehicle class is " + vclass);
 		System.out.println("Vehicle make name: " + make);
 		System.out.println("Vehicle model name: " + model);
 		System.out.println("Vehicle model year: " + year);
@@ -106,13 +93,5 @@ public class Vehicle {
 		System.out.println("Vehicle color is: " + color);
 		System.out.println("Vehicle EPA rated MPG: " + mpg);
 		System.out.println(notes);
-
-
 	}
-
-
-
-
-
-
 }
